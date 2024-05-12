1f326e2979f7b794273f37774b4206c392979214
KitsuneKode <bhuyanmanash2002@gmail.com>
2024-05-12 15:56:42 +0530

    added zsh files

---
+++ zsh/.fzf.zsh
@@ -0,0 +1,7 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */home/kitsunekode/.fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/home/kitsunekode/.fzf/bin"
+fi
+
+eval "$(fzf --zsh)"
---
+++ zsh/.zlogout
@@ -0,0 +1,1 @@
+rm ~/.neofetch_has_run
